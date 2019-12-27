d358f650aa361c46a86445364d56fcd383d0bb96
Sundeep Chand <sundeep.chand2001@gmail.com>
2019-12-28 00:57:42 +0530

    Updates

---
+++ precache-manifest.cec445cb3bf10e0c4e7cc238ad9d8f28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f856fb3c717bea3e0fa28eed5ccc55e",
+    "url": "/aahvaan_new/index.html"
+  },
+  {
+    "revision": "c72ae0742ec6fb68fe18",
+    "url": "/aahvaan_new/static/css/main.8590b9dd.chunk.css"
+  },
+  {
+    "revision": "bfde4910c73af066f65f",
+    "url": "/aahvaan_new/static/js/2.0453498f.chunk.js"
+  },
+  {
+    "revision": "6d8f057f12ab0a384ad50077f988ea07",
+    "url": "/aahvaan_new/static/js/2.0453498f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c72ae0742ec6fb68fe18",
+    "url": "/aahvaan_new/static/js/main.1029b431.chunk.js"
+  },
+  {
+    "revision": "629b721d49fd7cf77aa1",
+    "url": "/aahvaan_new/static/js/runtime-main.ff963023.js"
+  },
+  {
+    "revision": "de8a56e3016f50d3c37daebce07c42ac",
+    "url": "/aahvaan_new/static/media/logo.de8a56e3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aahvaan_new/precache-manifest.22482d1364138abc2d4c6ce46c58dc53.js"
+  "/aahvaan_new/precache-manifest.cec445cb3bf10e0c4e7cc238ad9d8f28.js"
 );
 
 self.addEventListener('message', (event) => {
